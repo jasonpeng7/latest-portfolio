6c66ebc3fed148da0668ad189543e8b8c9465c2e
jasonpeng7 <jiapeng@ucdavis.edu>
2025-06-05 02:07:58 -0700

    fix: fixed color issues

--- src/components/features/aboutmeCMPNT.jsx
+++ src/components/features/aboutmeCMPNT.jsx
@@ -1,18 +1,18 @@
 const TypeWriter = () => {
   return (
     <div className="flex flex-col gap-y-4 sm:gap-y-6 md:gap-y-8 max-w-[600px] text-dark-navy">
-      <p className="red-hat text-sm sm:text-md md:text-lg ">
+      <p className="red-hat-normal text-sm sm:text-md md:text-lg ">
         I&apos;m Jason, and I discovered my passion for building right after
         college, even before I declared as a CS major. Once I started, I
         couldn’t stop.
       </p>
 
-      <p className="red-hat text-sm sm:text-md md:text-lg ">
+      <p className="red-hat-normal text-sm sm:text-md md:text-lg ">
         Right now, I&apos;m working on a mobile app to make roommate finding
         easier for over 40,000 students.
       </p>
 
-      <p className="red-hat text-sm sm:text-md md:text-lg ">
+      <p className="red-hat-normal text-sm sm:text-md md:text-lg ">
         In my free time, I love building side projects, exploring new
         technologies, and playing pickleball.
       </p>
