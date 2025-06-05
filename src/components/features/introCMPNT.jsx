ea23255520058658bdadc866e808da9cca74db12
jasonpeng7 <jiapeng@ucdavis.edu>
2025-06-05 02:08:38 -0700

    fix: color issues

--- src/components/features/introCMPNT.jsx
+++ src/components/features/introCMPNT.jsx
@@ -22,14 +22,14 @@ export function Intro() {
 
   return (
     <div className="flex items-center justify-center w-full lg:flex-row flex-col px-10">
-      <div className="flex flex-col items-center md:items-start lg:items-start xl:items-start 2xl:items-start space-y-4 text-left">
+      <div className="flex flex-col items-center md:items-start lg:items-start xl:items-start 2xl:items-start space-y-4 text-left ">
         <h2
           className="red-hat-extrabold bg-clip-text text-transparent bg-dark-navy 
         text-5xl md:text-5xl lg:text-6xl 2xl:text-7xl py-2 md:py-10 tracking-tight"
         >
           My name is, <br /> Jason Peng.
         </h2>
-        <p className="red-hat text-dark_navy text-center md:text-left lg:text-left xl:text-left 2xl:text-left max-w-xl text-sm md:text-lg">
+        <p className="red-hat-normal text-neutral-grey-3 text-center md:text-left lg:text-left xl:text-left 2xl:text-left max-w-xl text-sm md:text-lg ">
           Fullstack developer and passionate computer science student in love
           with building impactful software.
         </p>
